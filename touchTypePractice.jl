63775d49a1f891558b44c019ae65e05fafe6b214
Alex Hiller <xandhiller@gmail.com>
2018-11-19 19:37:38 +1100

    Game is good.

--- touchTypePractice.jl
+++ touchTypePractice.jl
@@ -1,9 +1,26 @@
-# using Plots
-#unicodeplots()
-using UnicodePlots
-using Logging
-# logFile = "runLog.txt" # For future use
-SimpleLogger(stdout, Logging.Debug)
+# TODO:
+# ✓ Timing of input -- how long did it take?
+# Words per minute calculation
+# If errors, how many errors?
+# Letters per minute calculation
+# Capture backspaces?
+# Measure error?
+# Log file to measure progress
+# Progress chart
+
+################################################################################
+# BEGIN LIBRARIES and OPEN FILES
+################################################################################
+
+#using UnicodePlots
+#using CSV
+#using DataFrames
+words = open("words.txt", "r")
+scoreFile = open("scores.txt", "a") 
+
+################################################################################
+# BEGIN FUNCTIONS
+################################################################################
 
 # Print paragraph to stdout
 function displayParagraph(para)
@@ -17,13 +34,11 @@ end
 # Calculate number of words in response.
 function nbCorrect(benchmark, attempt)
   count = 0
+  c = 0
   a = split(attempt)
   b = benchmark
-#   display(count)
-#   display(a)
-#  display(b)
-
-  for i ∈ 1:length(a)
+  a ≥  b ? c = b : c = a
+  for i ∈ 1:length(c)
     if a[i] == b[i]
       count += 1
     end
@@ -37,29 +52,30 @@ function wpm(scr, timeSecs)
   return scr/timeMins
 end
 
-words = open("words.txt", "r")
+function beginScores()
+  println("\n\nSCORES: \n",
+    "------------------------------------------------------------------")
+end
+
+function endScores()
+  println("------------------------------------------------------------------")
+end
+
+################################################################################
+# BEGIN MAIN
+################################################################################
+
+# Input for paragraph generation.
 lines = readlines(words)
 
 # Number of words to use in the game
-numWords = 100
-paragraph = []
+numWords = 20
 
 # Randomly sample some words
+paragraph = []
 for i ∈ 1:numWords
   push!(paragraph, rand(lines))
 end 
-
-#@info "The paragraph generated is: " display(paragraph)
-
-# TODO:
-# ✓ Timing of input -- how long did it take?
-# Words per minute calculation
-# If errors, how many errors?
-# Letters per minute calculation
-# Capture backspaces?
-# Measure error?
-# Log file to measure progress
-# Progress chart
 
 # Running shell command to clear text
 cmd = `clear`
@@ -71,15 +87,53 @@ displayParagraph(paragraph)
 
 # Take in the reponse and time it.
 println("\n\nCopy out the paragraph using touch-typing.\n")
+println("Press ENTER to begin:\n\n")
+ready = readline() 
+while ready != ""
+  ready = readline()
+end
+println("Timer has begun.\n\n")
 t = @elapsed response = readline()
 
 # Raw number of words input
 rawWordLength = length(split(response))
+
+# BEGIN SCORES
+beginScores()
 println("Number of words input is: " * string(rawWordLength) * "\n")
 
 # Number of correct words
 score = nbCorrect(paragraph, response)
-println("Accuracy score is: " * string(score) * "\n")
+#println("Accuracy score is: " * string(score/length(split(response))))
 
 # Words per minute calculation
-println("Words per minute: " * string(wpm(score, t)))
+println("Words per minute: " * string(wpm(score, t))) 
+
+# Calculate total word error.
+error = ((length(split(response)) - score)/float(length(split(response))))*100.0
+println("Error is: " * string(error) * "%" )
+
+# END SCORES
+endScores()
+
+# Save scores
+write(scoreFile, 
+      string(error)*",", 
+      string(wpm(score, t))*",", 
+      string(rawWordLength),
+     "\n")
+
+# Close files and cat out scores to terminal. 
+close(words)
+close(scoreFile)
+#df = DataFrame(CSV.File("scores.txt"))
+#display(tail(df))
+#print("\n")
+
+# Load score data and plot it
+#x1 = collect(1:1:length(df[1]))
+#x2 = collect(1:1:length(df[2]))
+#x3 = collect(1:1:length(df[3]))
+#lineplot(x1,  convert(Array{Float64}, df[1]))
+#lineplot!(x2, convert(Array{Float64}, df[2]))
+#lineplot!(x3, convert(Array{Float64}, df[3]))
