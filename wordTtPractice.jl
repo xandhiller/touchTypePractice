e511fd354f47696042b0e0b00aafb7eb1ede49ec
xandhiller <xandhiller@gmail.com>
2018-12-08 16:01:32 +1100

    Colouring of text now present.

--- wordTtPractice.jl
+++ wordTtPractice.jl
@@ -11,33 +11,28 @@
 ################################################################################
 # OPEN FILES
 ################################################################################
-
 words = open("words.txt", "r")
 scoreFile = open("wordScores.txt", "a") 
 
 ################################################################################
 # BEGIN FUNCTIONS
 ################################################################################
-
 # Print paragraph to stdout
 function displayParagraph(para)
   for i ∈ 1:length(para)
     typeof(para)
-    print(para[i], " ")
+    printstyled(para[i], " ", bold=:bold)
   end
-  println()
 end
 
 # Calculate number of words in response.
 function nbCorrect(benchmark, attempt)
-
   count = 0
   c = 0
   a = split(attempt)
   b = benchmark
   # Need to account for b ≥ a or vice versa ⇒ indexing error
   length(a) ≥ length(b) ? c = b : c = a
-
   for i ∈ 1:length(c)
     if a[i] == b[i]
       count += 1
@@ -61,61 +56,55 @@ function endScores()
   println("------------------------------------------------------------------")
 end
 
+
 ################################################################################
 # BEGIN MAIN
 ################################################################################
-
 # Input for paragraph generation.
 lines = readlines(words)
-
 # Number of words to use in the game
-numWords = 20
-
+numWords = 10
+printScores = false
 # Randomly sample some words
 paragraph = []
 for i ∈ 1:numWords
   push!(paragraph, rand(lines))
 end 
-
 # Running shell command to clear text
 cmd = `clear`
 run(cmd)
-
 # Display the paragraph
-println("Type out this paragraph: \n")
+printstyled("Touch-type the paragraph below [Press ENTER to begin timer]\n\n", 
+            color=:yellow)
 displayParagraph(paragraph)
-
 # Take in the reponse and time it.
-println("\n\nCopy out the paragraph using touch-typing.\n")
-println("Press ENTER to begin:\n\n")
 ready = readline() 
 while ready != ""
   ready = readline()
 end
-println("Timer has begun.\n\n")
+printstyled("\nTimer [START].\n\n", color=:cyan)
 t = @elapsed response = readline()
-
+printstyled("\nTimer [END].\n\n", color=:cyan)
 # Raw number of words input
 rawWordLength = length(split(response))
-
-beginScores()
-println("Number of words input is: " * string(rawWordLength) * "\n")
-# Number of correct words
 score = nbCorrect(paragraph, response)
-# Words per minute calculation
-println("Words per minute: " * string(wpm(rawWordLength, t))) 
-# Calculate total word error.
 error = ((length(split(response)) - score)/float(length(split(response))))*100.0
-println("Error is: " * string(error) * "%" )
-endScores()
-
+wpm_ = wpm(rawWordLength, t)
+if printScores == true
+  beginScores()
+  println("Number of words input is: " * string(rawWordLength) * "\n")
+  # Number of correct words
+  # Words per minute calculation
+  println("Words per minute: " * string(wpm_)) 
+  println("Error is: " * string(error) * "%" )
+  endScores()
+end
 # Save scores
 write(scoreFile, 
       string(error)*",", 
       string(wpm(score, t))*",", 
       string(rawWordLength),
      "\n")
-
 # Close files and cat out scores to terminal. 
 close(words)
 close(scoreFile)
